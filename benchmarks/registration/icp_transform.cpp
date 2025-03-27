5dde2f277c48b47b97be4dfa090cc7e52abc8d9b
Lars Glud <larshg@gmail.com>
2025-03-27 12:36:03 +0100

    Added benchmark for icp transform.

---
+++ benchmarks/registration/icp_transform.cpp
@@ -0,0 +1,70 @@
+#include <pcl/registration/icp.h>
+#include <pcl/common/generate.h>
+
+#include <benchmark/benchmark.h>
+
+using namespace pcl::common;
+
+
+class ICPTestHelper : public pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> {
+public:
+  using IterativeClosestPoint::transformCloud;
+};
+
+static void
+BM_ICPTransform(benchmark::State& state)
+{
+  // Perform setup here
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+  const auto pointCount = state.range(0);
+
+  CloudGenerator<pcl::PointXYZ, UniformGenerator<float>> generator;
+  auto seed = static_cast<std::uint32_t>(time(nullptr));
+  UniformGenerator<float>::Parameters x_params(-5.0, 5.0, seed++);
+  generator.setParametersForX(x_params);
+  UniformGenerator<float>::Parameters y_params(-5.0, 5.0, seed++);
+  generator.setParametersForY(y_params);
+  UniformGenerator<float>::Parameters z_params(5.0, 5.0, seed++);
+  generator.setParametersForZ(z_params);
+
+  generator.fill(pointCount, 1, *cloud);
+
+  ICPTestHelper icpTestHelper;
+
+  Eigen::Affine3f transform = Eigen::Affine3f::Identity();
+  transform = transform.rotate(Eigen::AngleAxisf(0.1, Eigen::Vector3f::UnitZ()));
+  transform.translation() << 0.1, 0.1, 0.1;
+  
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(
+      new pcl::PointCloud<pcl::PointXYZ>);
+  cloud_transformed->resize(cloud->size());
+  for (auto _ : state) {
+    // This code gets timed
+    icpTestHelper.transformCloud(
+        *cloud, *cloud_transformed, transform.matrix());
+  }
+}
+
+
+int
+main(int argc, char** argv)
+{
+
+  if (argc < 3) {
+    std::cerr
+        << "No test files given. Please download `table_scene_mug_stereo_textured.pcd` "
+           "and `milk_cartoon_all_small_clorox.pcd`, and pass their paths to the test."
+        << std::endl;
+    return (-1);
+  }
+
+  benchmark::RegisterBenchmark("BM_icp_transform", &BM_ICPTransform)
+      ->RangeMultiplier(10)
+      ->Range(100, 10000000)
+      ->Unit(benchmark::kMillisecond);
+
+  benchmark::Initialize(&argc, argv);
+  benchmark::RunSpecifiedBenchmarks();
+}
